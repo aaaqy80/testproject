de87ef251a78b23212b2702e1411ad23a7f5ff4e
aaaqy86wxz <1412648415@qq.com>
2018-11-28 14:14:38 +0800

    Initial commit

---
+++ src/main/java/com/aaa/dao/TestDao.java
@@ -0,0 +1,13 @@
+package com.aaa.dao;
+
+/**
+ * className:TestDao
+ * discriptoin:
+ * author:zz
+ * createTime:2018-11-28 14:05
+ */
+public class TestDao {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
